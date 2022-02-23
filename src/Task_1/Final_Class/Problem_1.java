82f40a33286dbf94bc87e4ce7732a7a88c42dc42
Vaishnavi8055 <vaishnavi.chaurasia_cs18@gla.ac.in>
2022-02-23 10:53:13 +0530

    Java Concepts

---
+++ src/Task_1/Final_Class/Problem_1.java
@@ -0,0 +1,21 @@
+package Task_1.Final_Class;/*
+ * Created by Vaishnavi Chaurasia
+ * 23-Feb-22
+ * 10:45 AM
+ * IoT83 Internship
+ */
+
+/** final class - can't be inherited **/
+public final class Problem_1 {
+
+    /** Final variable -  can't be modified **/
+    final double PIE = 3.14;
+
+    /** Final method -  can't be override **/
+    final void display()
+    {
+        System.out.println("This is a final method.");
+    }
+
+
+}
